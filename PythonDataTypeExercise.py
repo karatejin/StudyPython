f58c6e57573578c2bc9c5b77d17e80e2acb1f0c1
karatejin <junsyang@naver.com>
2023-02-20 22:23:01 +0900

    2023-02-20-23:22

---
+++ PythonDataTypeExercise.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+
+new_two_digit_number = (int(two_digit_number[0])+int(two_digit_number[1]))
+
+print(new_two_digit_number)
+
+#####################################
+# 다른 스타일로 하기
+
+# first_digit = two_digit_number[0]
+# second_digit = two_digit_number[1]
+
+# result = int(first_digit) + int(second_digit)
+# print(result)
---
+++ studyPy0220.py
@@ -0,0 +1,12 @@
+# num_char = len(input("What is your name?"))
+
+# new_num_char = str(num_char) # num_char 를 문자열로 변환 하고 새 이름인 new_num_char에 저장
+
+# print("Your name has " + new_num_char + " characters.") # 새 변수인 new_num_char를 출력문에 넣음.
+
+a = float(123)
+print(type(a))
+
+# print(70 + float("100.5"))
+
+print(str(70)+str(100))
