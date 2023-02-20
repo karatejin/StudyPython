69b4468e72708f0d8e93371046cf543a9a32038f
DESKTOP-MEHT3HB\USER <junsyang@naver.com>
2023-02-20 11:45:40 +0900

    study py first

---
+++ study.py
@@ -0,0 +1,6 @@
+a = input("hello what is you name?" + "/ your Anser : ")
+str = input("Hello? " + a + " how are you?" + "/ your Anser : ")
+str1 = input("i'm fine too thank you, do you have any question?" + "/ your Anser : ")
+str2 = input("it's sunny day! hope to take SunGlasses and take SunBlocks" + "/ your Anser : ")
+ans = "see ya"
+print((ans))
