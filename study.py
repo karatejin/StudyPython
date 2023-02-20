3020f86182d80fc5ae2e41563523596c96007a74
karatejin <junsyang@naver.com>
2023-02-20 16:22:33 +0900

    second

--- study.py
+++ study.py
@@ -1,6 +1,53 @@
-a = input("hello what is you name?" + "/ your Anser : ")
-str = input("Hello? " + a + " how are you?" + "/ your Anser : ")
-str1 = input("i'm fine too thank you, do you have any question?" + "/ your Anser : ")
-str2 = input("it's sunny day! hope to take SunGlasses and take SunBlocks" + "/ your Anser : ")
-ans = "see ya"
-print((ans))
+# a = input("hello what is you name?" + "/ your Anser : ")
+# str = input("Hello? " + a + " how are you?" + "/ your Anser : ")
+# str1 = input("i'm fine too thank you, do you have any question?" + "/ your Anser : ")
+# str2 = input("it's sunny day! hope to take SunGlasses and take SunBlocks" + "/ your Anser : ")
+# ans = "see ya"
+# print((ans))
+
+# Variables 변수
+
+# name = "Jack"
+# print=(name)
+
+# name = "Angela"
+# print(name)
+
+# name = input("What is your name?")
+# length = len(name)
+# print(length)
+
+# name  = input("What is your name?")
+# print(len(name))
+
+# 🚨 Don't change the code below 👇
+# a = input("a: ")
+# b = input("b: ")
+# # 🚨 Don't change the code above 👆
+
+# ####################################
+# #Write your code below this line 👇
+
+# # 간단하게 컵에 서로다른 액체를 옮기려고 하면 다른 컵을 가져와서 액체를 옮겨야 한다.
+# c = a
+# a = b
+# b = c
+
+# #Write your code above this line 👆
+# ####################################
+
+# # 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
+
+
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator")
+#2. Ask the user for the city that they grew up in
+question1 = input("which city did you grew up in? \n")
+#3. Ask the user for the name of a pet.
+question2 = input("what is your pet name? \n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + question1 + " " + question2)
+
+#5. Make sure the input cursor shows on a new line:
