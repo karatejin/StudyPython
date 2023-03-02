a27487c49d49e6957ee621b54a4f907069393dbe
karatejin <junsyang@naver.com>
2023-03-02 19:27:16 +0900

    2023-02-28 함수

---
+++ 2023-02-28/function.py
@@ -0,0 +1,14 @@
+## Functions
+
+# print() ## ()가 들어가는 것은 함수이다.
+# num_char = len("Hello") ## len() 함수
+
+## 
+def my_function():  ## 내가 설정한 함수 def blabla()
+	a = 3
+	if a > 2:
+		print("This is will run")
+    
+my_function() ## 함수 호출
+
+
