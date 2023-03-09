abcba31b55ee9b6281098b09bf3bc2962cda7c00
karatejin <junsyang@naver.com>
2023-03-10 08:39:20 +0900

    유저 실패시 컴퓨터가 선택한 단어 보여주기 추가

--- 2023-03-02/hangmanStudy5.py
+++ 2023-03-02/hangmanStudy5.py
@@ -50,7 +50,7 @@ while not end_of_game:
         lives -= 1
         if lives == 0:
             end_of_game = True
-            print("You lose")
+            print(f"You lose!! answer is {chosen_word}")
     print(f"{' '.join(display)}")
 
     if "_" not in display:
